1e8d0ac4dfc5549fb0a65d744cafa2d7002420e6
Theophane <theophane.kouchoanou@ccsd.cnrs.fr>
2021-07-24 17:15:42 +0200

    remplissage ingredient list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import {Cocktail} from "./shared/interfaces/cocktail.interface";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- cocktail: Cocktail={
-   name:'Mojito',
-   img:'https://maisonfoody.com/sites/default/files/styles/article_paragraph_image/public/2020-08/articles-images-corps-texte-8.png?itok=tDVVevZ-',
-   description:'Il est sans conteste le cocktail préféré des français et probablement l’un des plus faciles à réaliser. Le Mojito, traditionnellement composé de 4 à 6 cl de rhum blanc, d’eau gazeuse, de 3 cuillères à café de sucre de canne, d’1/2 citron vert, de glaçons et de feuilles de menthe, peut aussi se dévergonder avec du jus de fraise par exemple ! Il se mariera à la perfection avec un délicieux guacamole au piment doux. '
- };
 }
--- src/app/panier-container/ingredient-list/ingredient-list.component.ts
+++ src/app/panier-container/ingredient-list/ingredient-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, Input, OnDestroy} from '@angular/core';
+import { Ingredient } from "../../shared/interfaces/ingredient.interface";
+import {PanierService} from "../../shared/service/panier.service";
+import {Subscription} from "rxjs";
 @Component({
   selector: 'app-ingredient-list',
   templateUrl: './ingredient-list.component.html',
   styleUrls: ['./ingredient-list.component.scss']
 })
-export class IngredientListComponent implements OnInit {
+export class IngredientListComponent implements OnInit,OnDestroy {
+  @Input() public ingredients: Ingredient[] |null=null;
+  private subscription:Subscription=new Subscription();
+  constructor(private panierService:PanierService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    let s1=this.panierService.ingredients$.subscribe((ingredients)=>this.ingredients=ingredients)
+    this.subscription.add(s1);
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
