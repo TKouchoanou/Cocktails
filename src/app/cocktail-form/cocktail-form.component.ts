9fa1b91c60379d760ae3c9c5fc72dc15b9f34db3
Theophane <theophane.kouchoanou@ccsd.cnrs.fr>
2021-07-26 01:07:05 +0200

    fix pour remplissage du default ingredient

--- src/app/cocktail-form/cocktail-form.component.ts
+++ src/app/cocktail-form/cocktail-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {CocktailService} from "../shared/service/cocktail.service";
 import {Cocktail} from "../shared/interfaces/cocktail.interface";
@@ -25,17 +25,15 @@ public cocktail!:Cocktail;
     });
 
   }
+
   get ingredients(){
     return this.cocktailForm.get('ingredients') as FormArray
   }
+
   public addIngredient(): void {
-    this.ingredients.push(new FormGroup(
-      {
-              'name':new FormControl('',Validators.required),
-               'quantity':new FormControl('',[Validators.required,Validators.min(0)])
-              }
-              ))
+    this.ingredients.push(this.ingredientToFormGroup())
   }
+
   public submit(): void {
     let index=this.cocktailService.addCocktail(this.cocktailForm.value);
     console.log(index)
@@ -50,10 +48,10 @@ public cocktail!:Cocktail;
     })
   }
 
-  public  ingredientToFormGroup(ingredient:Ingredient={name:'pom',quantity:1}){
+  public  ingredientToFormGroup(ingredient:Ingredient={name:'rhum',quantity:1}){
    return  this.fb.group({name:[ingredient.name,Validators.required],
                                quantity:[ingredient.quantity,[Validators.required,Validators.min(0)]]
-                               }) as FormGroup
+                               })
   }
 //Pina Colada
 // Dans un shaker, versez les ingrédients en commençant par la dose de rhum blanc Old Nick puis en rajoutant successivement les autres. Secouez énergiquement pendant une minute afin de bien les mélanger. Servez votre cocktail dans un grand verre, sur un lit généreux de glaçons, en ayant pris soin de soigner la décoration (feuille de menthe ou morceau d’ananas coupé planté sur une pique en bois). Dégustez votre Piña Colada très fraîche.
